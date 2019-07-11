d3b2c8953dbd10198f44340e72e4bd780b43c737
shenbanakshetha24 <51950533+shenbanakshetha24@users.noreply.github.com>
2019-07-11 22:24:22 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,12 @@
+gh=int(input())
+df=[]
+for i in range90,gh):
+ ij=input()
+ df.append(io)
+ff=[]
+for i in dairy(*df):
+ if(i.count(i[0])==len(i)):
+  ff.append(i[0])
+ else:
+  break
+print(".join(ff))
