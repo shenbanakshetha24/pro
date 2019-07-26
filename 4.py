3641e042fc9792438934ff73961334a335bd97c5
shenbanakshetha24 <51950533+shenbanakshetha24@users.noreply.github.com>
2019-07-26 09:37:47 -0700

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,11 @@
+df,gh=map(str,input().split())
+i=0
+if len(df)>len(gh):
+ df,gh=gh,df
+j=0
+while j<len(df):
+ i+=(ord(gh[j]-ord(df[j]))
+ j+=1
+for j in range(j,len(df)):
+ i+=ord(df[j]-ord('a')+1
+print(i)
